c6a37d323b4c8de513cf13573e47c0becab30aef
jhen852 <jhen852.jc@gmail.com>
2020-12-19 16:36:45 -0600

    version beta

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package holamundo;
+
+/**
+ *
+ * @author Jhen
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+       System.out.println("Hola mundo");
+        System.out.print("Jhenny");
+        System.out.println("Jimena");
+    }
+    
+}
