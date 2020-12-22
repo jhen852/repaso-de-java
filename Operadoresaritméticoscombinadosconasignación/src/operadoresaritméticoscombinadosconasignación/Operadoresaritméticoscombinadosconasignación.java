7bfdcaa29bb81e7b291a4833b0453f27a375e58d
jhen852 <jhen852.jc@gmail.com>
2020-12-22 00:47:18 -0600

    version 5

--- Operadoresaritméticoscombinadosconasignación/src/operadoresaritméticoscombinadosconasignación/Operadoresaritméticoscombinadosconasignación.java
+++ Operadoresaritméticoscombinadosconasignación/src/operadoresaritméticoscombinadosconasignación/Operadoresaritméticoscombinadosconasignación.java
@@ -1,16 +1,41 @@
 package operadoresaritméticoscombinadosconasignación;
 import java.util.Scanner;
+//CLASE MATH
 /**
  *
  * @author Jhen
  */
 public class Operadoresaritméticoscombinadosconasignación {
     public static void main(String[] args) {
-       int x = 5;
-       x+=1;
-       x++;
-       x--;
-        System.out.println(""+x);
+     double raiz = Math.sqrt(36.67);
+     //MATH.sqrt nos da un resultado DOUBLE
+      // Podemos hacer un numero random
+      
+      //tenemos que buscar, ordenar, eliminar
+      
+      // sacar la raiz cuadrada 
+        System.out.println("la raiz es "+raiz);
+        
+        double base = 5 , exponente = 2;
+        System.out.println("Sacar el exponencial");
+        double exponencial = Math.pow(base, exponente);
+        System.out.println("exponencial es "+exponencial);
+        
+        // vamos a redondear un numero
+        double  numero = 4.56;
+        long  resultado = Math.round(numero);
+        // tenemos que ver algunos otros casos mas 
+        
+        //round que es redondeo hay dos opciones las cuales son 
+        // float y double 
+        /*
+        
+        
+        
+        */
+        
+        
+        
     }
     
 }
